6ffb6a3e121aa242ba4866c2dc7c10199b266a5f
Bruno Soares dos santos <83316195+BrunoSoaresdosSantos@users.noreply.github.com>
2023-01-05 10:13:26 -0300

    Add files via upload

---
+++ Hublab/conexao/db.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('softex', 'root', '', { dialect: 'mysql', host: 'localhost' });
+
+sequelize.authenticate().then(function () {
+    console.log("conexão realiazda com sucesso")
+}).catch(function (error) {
+    console.log("não está pegando " + error)
+})
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
