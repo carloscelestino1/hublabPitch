2977949b19dec2ece881a94abf2b8a5422b2bb98
Carlos Celestino <49007553+carloscelestino1@users.noreply.github.com>
2023-01-10 11:43:25 -0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,9 +1,7 @@
-from django.db import models
-
 class PessoaF(models.Model):
     nomecomp = models.CharField(max_length=255)
     email = models.EmailField()
-    telefone = models.IntegerField()
+    telefone = models.CharField(max_length=15)
     
     def __str__(self):
         return self.nomecomp
